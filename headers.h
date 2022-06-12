b8d8e81a27cc0744863e59c5f4a08d42f3db39cb
poojith7 <mannepalli.e0219053@sret.edu.in>
2022-06-12 22:26:37 +0530

    Add files via upload

---
+++ headers.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<dirent.h>
+#include<errno.h>
+#include <sys/stat.h> 
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include<signal.h>
